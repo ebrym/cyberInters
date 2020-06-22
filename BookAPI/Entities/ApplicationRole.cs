6b4620b435efc957a87fc133f542c8ce28175e56
Abdullahi Ibrahim <ibrodex@gmail.com>
2020-06-22 12:08:13 +0100

    Initial Commit

---
+++ BookAPI/Entities/ApplicationRole.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookAPI.Entities
+{
+    public class ApplicationRole : IdentityRole
+    {
+       
+    }
+}
---
+++ BookAPI/Interface/IUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using BookAPI.Entities;
+
+namespace BookAPI.Interface
+{
+    public interface IUser
+    {
+        User Authenticate(string username, string password);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+        User Create(User user, string password);
+        void Update(User user, string password = null);
+        void Delete(int id);
+    }
+}
